a093d381070640bca51c96810bc72bd895ebdb04
Mark Rai <mark.rai@ici.org>
2017-05-26 14:24:07 -0400

    make it bare bones first

--- src/main/java/my/vaadin/app/MyUI.java
+++ src/main/java/my/vaadin/app/MyUI.java
@@ -6,86 +6,74 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.server.FontAwesome;
+import com.vaadin.data.Property;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.shared.ui.ValueChangeMode;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.CssLayout;
-import com.vaadin.ui.Grid;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.ValoTheme;
+import com.vaadin.ui.*;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
+ * This UI is the application entry point. A UI may either represent a browser window
  * (or tab) or some part of a html page where a Vaadin application is embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be
  * overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("mytheme")
 public class MyUI extends UI {
-    
-    private CustomerService service = CustomerService.getInstance();
-    private Grid<Customer> grid = new Grid<>(Customer.class);
-    private TextField filterText = new TextField();
-    private CustomerForm form = new CustomerForm(this);
+
+    TextField testField = new TextField();
+    ComboBox testBox = new ComboBox();
+
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
-
-        filterText.setPlaceholder("filter by name...");
-        filterText.addValueChangeListener(e -> updateList());
-        filterText.setValueChangeMode(ValueChangeMode.LAZY);
-
-        Button clearFilterTextBtn = new Button(FontAwesome.TIMES);
-        clearFilterTextBtn.setDescription("Clear the current filter");
-        clearFilterTextBtn.addClickListener(e -> filterText.clear());
-
-        CssLayout filtering = new CssLayout();
-        filtering.addComponents(filterText, clearFilterTextBtn);
-        filtering.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
-
-        Button addCustomerBtn = new Button("Add new customer");
-        addCustomerBtn.addClickListener(e -> {
-            grid.asSingleSelect().clear();
-            form.setCustomer(new Customer());
-        });
-
-        HorizontalLayout toolbar = new HorizontalLayout(filtering, addCustomerBtn);
-
-        grid.setColumns("firstName", "lastName", "email");
-
-        HorizontalLayout main = new HorizontalLayout(grid, form);
-        main.setSizeFull();
-        grid.setSizeFull();
-        main.setExpandRatio(grid, 1);
-
-        layout.addComponents(toolbar, main);
-
-        // fetch list of Customers from service and assign it to Grid
-        updateList();
-
         setContent(layout);
 
-        form.setVisible(false);
+        Label testLabel = new Label();
 
-        grid.asSingleSelect().addValueChangeListener(event -> {
-            if (event.getValue() == null) {
-                form.setVisible(false);
-            } else {
-                form.setCustomer(event.getValue());
+
+        testBox.addValueChangeListener(new Property.ValueChangeListener() {
+            @Override
+            public void valueChange(Property.ValueChangeEvent valueChangeEvent) {
+                Notification.show("CHANGED");
             }
         });
-    }
 
-    public void updateList() {
-        List<Customer> customers = service.findAll(filterText.getValue());
-        grid.setItems(customers);
+
+        testLabel.setValue("My new label");
+        testLabel.setValue("another label!");
+        testField.setValue("my test field");
+
+        testBox.addItem("item1");
+        testBox.addItem("item2");
+        testBox.addItem("item3");
+        testBox.addItem("item4");
+
+        layout.addComponent(testLabel);
+        layout.addComponent(testField);
+        layout.addComponent(testBox);
+        layout.addComponent(testField);
+
+
+        HorizontalLayout hLayout = new HorizontalLayout();
+        Label label2 = new Label("Another test label: ");
+        label2.setWidth("200px");
+        hLayout.addComponent(label2);
+        TextField field2 = new TextField();
+
+
+        TextField field3 = new TextField();  //Really long text field
+        field3.setHeight("600px");
+        //hLayout.addComponent(field3);
+
+        hLayout.addComponent(field2);
+        layout.addComponent(hLayout);
+        label2.setValue("2");
+
+
+
+
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
@@ -93,3 +81,5 @@ public class MyUI extends UI {
     public static class MyUIServlet extends VaadinServlet {
     }
 }
+
+
